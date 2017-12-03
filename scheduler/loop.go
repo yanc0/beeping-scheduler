503ac5ee8f2c14543d94c20dbe2393eb657e7479
Yann Coleu <y@nn-col.eu>
2017-12-03 14:56:43 +0100

    initial commit

---
+++ scheduler/loop.go
@@ -0,0 +1,13 @@
+package scheduler
+
+// Scheduler is responsible of scheduling job when their
+// NextRun time has come
+type Scheduler struct {
+}
+
+// Run the main scheluler loop
+func (s *Scheduler) Run() {
+	for {
+
+	}
+}
